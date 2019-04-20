8e29737a67bd4f915c1b0f03bbe69f0a07b3e38f
Countero <javicg23@yahoo.com>
2019-04-20 17:40:45 +0200

    Vista hecha

---
+++ app/src/main/java/com/example/comprapp/crud/Anyadir.java
@@ -0,0 +1,15 @@
+package com.example.comprapp.crud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.comprapp.R;
+
+public class Anyadir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anyadir);
+    }
+}
